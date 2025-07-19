46d18bfc4715980986122427d4260a206f09164b
Piyush-codez0 <piyushcode.z1@gmail.com>
2025-07-19 13:57:53 +0530

    Data fetched from swiggy API

--- Food Delivery App/src/components/Body.jsx
+++ Food Delivery App/src/components/Body.jsx
@@ -1,23 +1,45 @@
 import RestaurantCard from "./RestaurantCard";
-import { res_data } from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-    const [restaurants, setRestaurants] = useState(res_data);
+  const [restaurants, setRestaurants] = useState([]);
+  const [allRestaurants, setAllRestaurants] = useState([]); // For filtering
 
-    const topRatedRest = () => {
-        const topRated = res_data.filter((data) => data.info.avgRating > 4);
-        setRestaurants(topRated);
-    };
+  // Fetching Data from Swiggy API
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fastDelivery = () => {
-        const fD = res_data.filter((data) => data.info.sla.deliveryTime < 30);
-        setRestaurants(fD);
-    };
+  const fetchData = async () => {
 
-    const reset = () => {
-      setRestaurants(res_data)
-    };
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.3445041&lng=77.8865854&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+      const json = await data.json();
+      const restaurantList = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+
+      setRestaurants(restaurantList);
+      setAllRestaurants(restaurantList);
+  };
+
+  // Filter Top Rated Restaurants (rating > 4)
+  const topRatedRest = () => {
+    const topRated = restaurants.filter(
+      (data) => data.info.avgRating > 4
+    );
+    setRestaurants(topRated);
+  };
+
+  // Filter fast delivery restaurants (DeliveryTime < 30min.)
+  const fastDelivery = () => {
+      const fD = allRestaurants.filter(
+        (data) => data.info.sla.deliveryTime < 30);
+      setRestaurants(fD);
+  };
+
+  // Reset btn to reset filters
+  const reset = () => {
+    setRestaurants(allRestaurants)
+  };
 
   return (
     <div id="main">
@@ -28,12 +50,29 @@ const Body = () => {
       </div>
 
       <div id="res-container">
-        {restaurants.map((res_data) => (
-          <RestaurantCard
-            key={res_data.info.id}
-            restaurants={res_data}
-          />
-        ))}
+        {restaurants.length === 0 ? (
+          <div className="shimmer-container">
+          <Shimmer />
+          <Shimmer />
+          <Shimmer />
+          <Shimmer />
+          <Shimmer />
+          <Shimmer />
+          <Shimmer />
+          <Shimmer />
+          <Shimmer />
+          <Shimmer />
+          <Shimmer />
+          <Shimmer />
+          </div>
+        ) : (
+          restaurants.map((data) => (
+            <RestaurantCard
+              key={data.info.id}
+              restaurants={data}
+            />
+          ))
+        )}
       </div>
     </div>
   );
